72c2a26a43989da23b9c323454bc751b30529770
SiddhantSonkar <124968287+SiddhantSonkar@users.noreply.github.com>
2023-04-11 09:25:23 +0530

    Add files via upload

---
+++ pounter02.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+	int x=4;
+	int y=1049;
+	int *ptr=&x;
+	printf("\nx = %d",x);
+	printf("\ny = %d",y);
+	printf("\n&x = %d",&x);
+	printf("\nptr = %d",*ptr);
+	printf("\n*y = %d",*&y);
+	
+	
+	
+	
+	
+}
